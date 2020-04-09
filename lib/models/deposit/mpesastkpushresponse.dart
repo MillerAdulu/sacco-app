c20cd775ed385a6a6241761e37484e1839deb8d0
Miller Adulu <milleradulu@gmail.com>
2020-04-09 23:55:30 +0300

    Remove generated filesgit

--- lib/models/deposit/mpesastkpushresponse.dart
+++ lib/models/deposit/mpesastkpushresponse.dart
@@ -5,14 +5,27 @@ import 'package:built_value/serializer.dart';
 
 part 'mpesastkpushresponse.g.dart';
 
-abstract class MpesaSTKResponse implements Built<MpesaSTKResponse, MpesaSTKResponseBuilder> {
-  String get MerchantRequestID;
-  String get CheckoutRequestID;
-  String get ResponseCode;
-  String get ResponseDescription;
-  String get CustomerMessage;
+abstract class MpesaSTKResponse
+    implements Built<MpesaSTKResponse, MpesaSTKResponseBuilder> {
+  @BuiltValueField(wireName: 'MerchantRequestID')
+  String get merchantRequestID;
+
+  @BuiltValueField(wireName: 'CheckoutRequestID')
+  String get checkoutRequestID;
+
+  @BuiltValueField(wireName: 'ResponseCode')
+  String get responseCode;
+
+  @BuiltValueField(wireName: 'ResponseDescription')
+  String get responseDescription;
+
+  @BuiltValueField(wireName: 'CustomerMessage')
+  String get customerMessage;
 
   MpesaSTKResponse._();
-  factory MpesaSTKResponse([updates(MpesaSTKResponseBuilder mpesaSTKResponseBuilder)]) = _$MpesaSTKResponse;
-  static Serializer<MpesaSTKResponse> get serializer => _$mpesaSTKResponseSerializer;
+  factory MpesaSTKResponse(
+          [updates(MpesaSTKResponseBuilder mpesaSTKResponseBuilder)]) =
+      _$MpesaSTKResponse;
+  static Serializer<MpesaSTKResponse> get serializer =>
+      _$mpesaSTKResponseSerializer;
 }
